1c790fbff22dea352cd4d47ee1c7963cdd5ab988
Sabina Cenda <sabina.cenda17@gmail.com>
2021-03-20 13:08:36 +0200

    modified main and added new class

--- src/loose/oose/fis/Main.java
+++ src/loose/oose/fis/Main.java
@@ -26,7 +26,7 @@ public class Main {
         ArrayList<Document> documente = new ArrayList<>();
         documente.add(xml);
 
-        Procesor c1 = new ProcesorCautare("text1");
+        Procesor c1 = new ProcesorCautare("text");
         Procesor c2 = new ProcesorCautare("text3");
         Procesor c3 = new ProcesorCautare("text2");
 
